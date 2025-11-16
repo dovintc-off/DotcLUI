4e41fa1c4e9ff2f4cbd943a1b3ed38ad24d82e23
dovintc-off <dovintc@gmail.com>
2025-11-16 03:32:09 +0300

    delite critical error

--- utils/color.lua
+++ utils/color.lua
@@ -1,29 +1,8 @@
 local json = require("DotcLUI.utils.dkjson")
 local color = {}
 
-function color.save(r, g, b, a)
-    color = {
-        math.max(0, math.min(1, r)),
-        math.max(0, math.min(1, g)),
-        math.max(0, math.min(1, b)),
-        math.max(0, math.min(1, a))
-    }
-    local data = {
-        color = color
-    }
-    local jsonString = json.encode(data, {indent = true})
-    local file = io.open("DotcLUI/utils/value.json", "w")
-    if file then
-        file:write(jsonString)
-        file:close()
-    else
-        print("Не удалось открыть value.json для записи")
-    end
-end
-
 function color.set(r, g, b, a)
     a = a or 1
-    color.save(r, g, b, a)
     love.graphics.setColor(r, g, b, a)
 end
 
